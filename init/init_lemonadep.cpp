8c9d1ef2f6733b07d1e1d06b0848bc4c3037c28e
Pranav Vashi <neobuddy89@gmail.com>
2021-09-17 21:55:50 +0530

    lemonadep: Rework init
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ init/init_lemonadep.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2021 crDroid Android Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t lemonadep_info = {
+    .brand = "OnePlus",
+    .device = "OnePlus9Pro",
+    .name = "OnePlus9Pro_EEA",
+    .model = "LE2123",
+    .build_fingerprint = "OnePlus/OnePlus9Pro_EEA/OnePlus9Pro:11/RKQ1.201105.002/2109102008:user/release-keys",
+};
+
+void vendor_load_properties() {
+    set_dalvik_heap();
+    set_variant_props(lemonadep_info);
+}
